e4ffd8cbe9c317151594669910433a838a2d2fcc
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-01-19 02:42:32 +0530

    created the sign-up page UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 export const HomePage = ()=>{
-    useEffect(()=>{
-        const res = axios.get("http://localhost:8000/api/v1/test");
-        console.log(res.data);
-    },[])
+    const [responseData, setResponseData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('http://localhost:8000/api/v1/user/test');
+        setResponseData(res.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
     return (
         <>
             <h1 className='text-3xl text-center  text-orange-700'>
                 HomePage
             </h1>
+            <p>{responseData && responseData.message ? JSON.stringify(responseData.message) : ""}</p>
 
         </>
     )
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import {Link} from "react-router-dom";
 
 export const SignUp = ()=>{
+
+    const handleRegister = async (e)=>{
+       e.preventDefault();
+    }
+
+
     return (
         <>
-            <h1 className='text-3xl text-center text-orange-700'>
-                SignUp
-            </h1>
+            <div className='p-3 max-w-lg mx-auto'>
+                <h1 className='text-3xl text-center font-semibold text-orange-700 my-7'>
+                    SignUp
+                </h1>
+                <form onSubmit={handleRegister} className='flex flex-col gap-4'>
+                    <input type="text" placeholder='Username' className='border rounded-lg p-3' id='username'/>
+                    <input type="email" placeholder='Email' className='border rounded-lg p-3' id='email'/>
+                    <input type="password" placeholder='Password' className='border rounded-lg p-3' id='password'/>   
+                    <button type="submit" 
+                        className='bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-70'>Sign-Up</button>
+                </form>
+                <div className='flex gap-1 mt-2'>
+                    <p>Have an account ?</p>
+                    <Link to="/sign-in" ><span className='text-blue-700 font-semibold'>Sign-In</span></Link>
+                </div>
+            </div>
+            
         </>
     )
 }
