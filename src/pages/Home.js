7aeeed8d134213dc638584c2246fd1f39866d4a8
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-01-18 21:31:22 +0530

    installed axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 export const HomePage = ()=>{
+    useEffect(()=>{
+        const res = axios.get("http://localhost:8000/api/v1/test");
+        console.log(res.data);
+    },[])
     return (
         <>
             <h1 className='text-3xl text-center  text-orange-700'>
                 HomePage
             </h1>
+
         </>
     )
 }
