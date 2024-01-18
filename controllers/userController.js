781eb2799b48217c50a664e710d287d8d716a1a2
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-01-18 22:46:34 +0530

    hashed the password using bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,14 @@
-const Users = require("../models/user")
+const Users = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
 module.exports.register = async (req,res)=>{
+    const {username, email, password} = req.body;
     try {
-        const user = await Users.create(req.body);
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const user  = await Users.create({username, email, password : hashedPassword});
         res.status(200).send({message : "User register successfuly", success : true})
     } catch (error) {
         res.status(404).send({message : "Error in resgistering the user", success :false});
-        console.log(`Register :  ${error}`);
+        console.log(`Register :  ${error.message}`);
     }
 }
