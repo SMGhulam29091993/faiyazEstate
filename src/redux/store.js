9f2ed1e643d6677756df223d3d6102863fda41a3
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-01-21 13:19:10 +0530

    stored the currentUser in localStorage using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,26 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
 import { userReducers } from "./user/userSlice";
+import {} from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({user : userReducers});
+
+const persistConfig = {
+    key : 'root',
+    storage,
+    version : 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer : {
-        user : userReducers,
-    },
+    reducer : persistedReducer,
     middleware: (getDefaultMiddleware)=>
     getDefaultMiddleware({
         serializableCheck : false,
     })
 })
+
+export const persistor = persistStore(store);
