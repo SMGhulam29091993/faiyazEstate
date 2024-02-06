78ce09e848aa07ec1cb132b8176dcafb3866dc43
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-02-06 16:25:06 +0530

    created the api for delete listing functionality and its router, and controller

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -7,4 +7,23 @@ module.exports.createList = async (req,res,next)=>{
     } catch (error) {
         next(error);
     }
+};
+
+module.exports.deleteList = async (req,res,next)=>{
+    const listings = await Listing.findById(req.params.id);
+    if(!listings){
+        res.status(401).send({message:"Sorry Listing Not Found", success : false});
+        return;
+    }
+    if(req.user.id !== listings.userRef){
+        res.status(401).send({message:"You are not authorized to delete this Listing", success : false});
+        return;
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).send({message:"Listing Deleted !!", success : true});
+    } catch (error) {
+        next(error);
+    }
+    
 }
--- routes/api/v1/listings.js
+++ routes/api/v1/listings.js
@@ -4,6 +4,6 @@ const listingController = require("../../../controllers/listingController.js");
 const {verifyUser} = require("../../../utils/verifyUser.js")
 
 router.post("/create-list", verifyUser, listingController.createList)
-
+router.post("/delete-listing/:id", verifyUser, listingController.deleteList)
 
 module.exports= router;
