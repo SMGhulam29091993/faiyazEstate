a08af989e47e09aff84f8a91ba776cc3ffd431db
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-02-17 19:17:23 +0530

    created the router and controller for search listings for search bar

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -56,4 +56,51 @@ module.exports.getListingDetails = async (req,res,next)=>{
     } catch (error) {
         next(error)
     }
+};
+
+module.exports.get_listings = async (req,res,next)=>{
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex)|| 0;
+
+        let offer = req.query.offer;
+        if(offer === undefined || offer === 'false'){
+            offer = {$in:[true,false]};
+        }
+
+        let furnished = req.query.furnished;
+        if(furnished === undefined || furnished=== 'false'){
+            furnished = {$in:[true,false]};
+        }
+
+        let parking = req.query.parking;
+        if(parking === undefined || parking === 'false'){
+            parking = {$in:[true,false]};
+        }
+
+        let type = req.query.type;
+        if(type === undefined || type === 'all'){
+            type = {$in:['sale', 'rent']};
+        }
+
+        const searchTerm = req.query.searchTerm || "";
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || "desc";
+
+        const listings = await Listing.find({
+            name : {$regex : searchTerm, $options : 'i'},
+            offer, 
+            furnished,
+            parking,
+            type
+        }).sort({[sort] : order}).limit(limit).skip(startIndex);
+
+        if(listings.length === 0){
+            res.status(401).send({message : "No Listing Found", success : false});
+            return;
+        }
+        res.status(200).send({message : "Here are your listings", success : true, listings});
+    } catch (error) {
+        next(error);
+    }
 }
--- routes/api/v1/listings.js
+++ routes/api/v1/listings.js
@@ -7,5 +7,5 @@ router.post("/create-list", verifyUser, listingController.createList)
 router.delete("/delete-listing/:id", verifyUser, listingController.deleteList)
 router.post("/update-listing/:id", verifyUser, listingController.updateListing)
 router.get("/get-listing-detail/:id", listingController.getListingDetails)
-
+router.get("/get",listingController.get_listings);
 module.exports= router;
